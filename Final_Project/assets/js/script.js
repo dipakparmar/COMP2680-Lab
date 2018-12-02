b6710f80fd1f26bf04da789d7f768574241be8b5
Dipak Parmar <dipakparmar65@gmail.com>
2018-12-02 15:17:39 -0800

    Added Blog Page

--- Final_Project/assets/js/script.js
+++ Final_Project/assets/js/script.js
@@ -14,32 +14,32 @@ var FTitle = " | " + SiteTitle;
 if(active_menu == "Home")
 {
     document.title = active_menu + FTitle;
-    document.getElementById("home").className += " active";
+    document.getElementById("home").classList.add('activemenu'); 
 }
 if(active_menu == "About")
 {
     document.title = active_menu + FTitle;
-    var setActiveClass = document.getElementById("about");
-    setActiveClass.className = setActiveClass.className.replace(" active", "");
+    document.getElementById("about").classList.add('activemenu');
+
 }
 if(active_menu == "Events")
 {
     document.title = active_menu + FTitle;
-    var setActiveClass = document.getElementById("events");
-    setActiveClass.className = setActiveClass.className.replace(" active", "");
+    document.getElementById("events").classList.add('activemenu');
+
 }
 if(active_menu == "Contact")
 {
     document.title = active_menu + FTitle;
-    var setActiveClass = document.getElementById("contact");
-    setActiveClass.className = setActiveClass.className.replace(" active", "");
+    document.getElementById("contact").classList.add('activemenu');
+  
 }
 
 if(active_menu == "Blog")
 {
     document.title = active_menu + FTitle;
-    var setActiveClass = document.getElementById("blog");
-    setActiveClass.className = setActiveClass.className.replace(" active", "");
+    document.getElementById("blog").classList.add('activemenu');
+    
 }
 
 
