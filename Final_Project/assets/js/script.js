1c26f11bf66c82062bcf3cf70006f548f8ebae7b
Dipak Parmar <dipakparmar65@gmail.com>
2018-12-02 13:55:23 -0800

    Fixed Navigation Toogle + Added Pages
    Contact Us Pages -- Done

--- Final_Project/assets/js/script.js
+++ Final_Project/assets/js/script.js
@@ -1,3 +1,5 @@
+window.onload=function()
+{
 
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -6,3 +8,42 @@ navBarToggle.addEventListener('click', function () {
     
     mainNav.classList.toggle('active');
 });
+
+var SiteTitle = "TRU LEAP";
+var FTitle = " | " + SiteTitle;
+if(active_menu == "Home")
+{
+    document.title = active_menu + FTitle;
+    document.getElementById("home").className += " active";
+}
+if(active_menu == "About")
+{
+    document.title = active_menu + FTitle;
+    var setActiveClass = document.getElementById("about");
+    setActiveClass.className = setActiveClass.className.replace(" active", "");
+}
+if(active_menu == "Events")
+{
+    document.title = active_menu + FTitle;
+    var setActiveClass = document.getElementById("events");
+    setActiveClass.className = setActiveClass.className.replace(" active", "");
+}
+if(active_menu == "Contact")
+{
+    document.title = active_menu + FTitle;
+    var setActiveClass = document.getElementById("contact");
+    setActiveClass.className = setActiveClass.className.replace(" active", "");
+}
+
+if(active_menu == "Blog")
+{
+    document.title = active_menu + FTitle;
+    var setActiveClass = document.getElementById("blog");
+    setActiveClass.className = setActiveClass.className.replace(" active", "");
+}
+
+
+
+
+
+}
