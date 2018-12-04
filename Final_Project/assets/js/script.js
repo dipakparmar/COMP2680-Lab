7bee38acea1641f390d87641df2f568a0b1e3132
Dipak Parmar <dipakparmar65@gmail.com>
2018-12-04 01:07:14 -0800

    Updated Events pages
    Fixed bug in amount calculations + formatted documents

--- Final_Project/assets/js/script.js
+++ Final_Project/assets/js/script.js
@@ -45,4 +45,9 @@ if(active_menu == "Blog")
 }
 
 
-   
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-87315467-6');
